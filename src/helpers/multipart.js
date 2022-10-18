e53c1539c69e45e8db57bb3cd5412dd9a0f9cbca
muchamad choirul anwar <anwaraan998@gmail.com>
2022-10-18 16:58:00 +0700

    filter upload

--- src/helpers/multipart.js
+++ src/helpers/multipart.js
@@ -1,12 +1,27 @@
 import multer from "multer";
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./assets/");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const unix = Date.now();
+    cb(null, unix + "-" + file.originalname);
   },
 });
-const upload = multer({ storage: storage });
+
+const filter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  }
+};
+
+const maxSize = 5 * 1024 * 1024;
+
+const upload = multer({ storage: storage, fileFilter: filter, limits: maxSize }).single("photo");
 
 export default upload;
--- src/routes/user.js
+++ src/routes/user.js
@@ -9,9 +9,9 @@ const router = Router();
 router.get("/", basicAuth, apiHandler.getUsers);
 router.get("/page", basicAuth, apiHandler.getUserPagination);
 router.get("/:userId", basicAuth, apiHandler.getuserById);
-router.post("/register", upload.single("photo"), basicAuth, apiHandler.registerUser);
+router.post("/register", basicAuth, upload, apiHandler.registerUser);
 router.post("/login", basicAuth, apiHandler.loginUser);
-router.put("/:userId", upload.single("photo"), jwtAuth, apiHandler.updateUser);
+router.put("/:userId", jwtAuth, upload, apiHandler.updateUser);
 router.delete("/:userId", jwtAuth, apiHandler.deleteUser);
 router.delete("/", basicAuth, apiHandler.deleteUsers);
 
